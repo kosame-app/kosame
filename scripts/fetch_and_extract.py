49659a9f60bfc5fe890298d1d2bbae00d0db4817
kosame-app <niseguchi@gmail.com>
2026-02-13 11:24:02 +0900

    Create fetch_and_extract.py

---
+++ scripts/fetch_and_extract.py
@@ -0,0 +1,25 @@
+import random
+import requests
+import json
+import re
+
+URL = "https://www.aozora.gr.jp/cards/000148/files/773_14560.html"
+
+html = requests.get(URL).text
+body = re.split("<body.*?>", html, flags=re.DOTALL)[1]
+body = re.split("</body>", body, flags=re.DOTALL)[0]
+
+text = re.sub("<.*?>", "", body)
+sentences = [s.strip() for s in text.split("。") if "雨" in s]
+
+sentence = random.choice(sentences)
+
+data = {
+    "sentence": sentence + "。",
+    "author": "夏目漱石",
+    "work": "こころ",
+    "app": "kosame"
+}
+
+with open("data/latest.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
